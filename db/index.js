df5e4fb122bb8121350533a8bb3e3e4f3f3641d1
Han <hl2341@nyu.edu>
2022-09-22 22:17:13 -0400

    4

--- db/index.js
+++ db/index.js
@@ -5,11 +5,9 @@ const conn = new Sequelize(process.env.DATABASE_URL);
 //postgres://database-name
 //postgres://rwadtudswxxriu:34e9501d167c77bcba4e3bca63a5a1885f7331b5cc35f8b0f8c32241eb307083@ec2-52-4-87-74.compute-1.amazonaws.com:5432/d1506j8lcvq4j4
 
-
 //add .env to .gitignore
 //const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/vercel-demo-db')
 //doesn't work on deployment because this is only locally connecting
-
 
 const Patient = conn.define("patient", {
   id: {
